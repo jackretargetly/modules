7d6b037e6e81de557b9427377d11d4d123fffadf
jack torpoco <jacktorpoco@jacks-MacBook-Pro.local>
2022-06-20 00:51:03 +0200

    update ingress and egress

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -9,6 +9,33 @@ resource "aws_security_group" "teampass" {
   vpc_id      = var.vpc_id
 }
 
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "http"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teampass.id
+}
+
+resource "aws_security_group_rule" "https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "http"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teampass.id
+}
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  security_group_id = aws_security_group.teampass.id
+}
+
+
 resource "aws_network_interface" "teampass" {
   subnet_id = var.create_subnet ? aws_subnet.teampass[0].id : data.aws_subnet.selected[0].id
   private_ips     = [var.teampass_privateip]
