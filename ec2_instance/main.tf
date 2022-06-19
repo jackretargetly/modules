f9703155a1c6e6b032c63d348f8129d0f0f79c12
jack torpoco <jacktorpoco@jacks-MacBook-Pro.local>
2022-06-19 18:57:49 +0200

    ok

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,13 +1,18 @@
-resource "aws_instance" "base" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+# resource "aws_instance" "base" {
+#   ami           = "ami-005e54dee72cc1d00" # us-west-2
+#   instance_type = "t2.micro"
 
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
+#   network_interface {
+#     network_interface_id = aws_network_interface.foo.id
+#     device_index         = 0
+#   }
 
-  credit_specification {
-    cpu_credits = "limited"
-  }
+#   credit_specification {
+#     cpu_credits = var.cpu_credits
+#   }
+# }
+
+resource "aws_subnet" "selected" {
+  vpc_id     = aws_vpc.selected.id
+  cidr_block = var.subnet_cdr
 }
--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,19 +1,41 @@
-variable "cpu_credits" {
-  type        = string
-  description = "Credit option for CPU usage"
+variable "vpc_id" {
+  type = string
 }
 
-variable "device_index" {
-  type        = string
-  description = "Integer index of the network interface attachment"
+variable "create_subnet" {
+  type = bool
+  default = 1
 }
 
-variable "instance_type" {
-  type        = string
-  description = "The instance type to use for the instance. Updates to this field will trigger a stop/start of the EC2 instance"
+variable "subnet_id" {
+  type = string
 }
 
-variable "ami" {
-  type        = string
-  description = "AMI to use for the instance"
+
+variable "subnet_cdr" {
+  type = string
 }
+
+
+
+
+
+# variable "cpu_credits" {
+#   type        = string
+#   description = "Credit option for CPU usage"
+# }
+
+# variable "device_index" {
+#   type        = string
+#   description = "Integer index of the network interface attachment"
+# }
+
+# variable "instance_type" {
+#   type        = string
+#   description = "The instance type to use for the instance. Updates to this field will trigger a stop/start of the EC2 instance"
+# }
+
+# variable "ami" {
+#   type        = string
+#   description = "AMI to use for the instance"
+# }
