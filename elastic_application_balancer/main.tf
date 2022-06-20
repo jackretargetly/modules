46d51d4d82628c49b0608127432b0d1163efcd3c
jack torpoco <jacktorpoco@jacks-MacBook-Pro.local>
2022-06-20 13:17:56 +0200

    updated data

--- elastic_application_balancer/main.tf
+++ elastic_application_balancer/main.tf
@@ -1,8 +1,41 @@
-resource "aws_lb" "test" {
+resource "aws_security_group" "allow_tls" {
+  name        = "lb_teampass"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.selected.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.selected.ipv6_cidr_block]
+  }
+
+  ingress {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.selected.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.selected.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+}
+
+resource "aws_lb" "retargetly" {
   name               = var.name
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
-  security_groups    = var.security_groups
-  subnets            = var.subnets_ids
+  security_groups    = [aws_security_group.allow_tls.id]
+  subnets            = data.aws_subnet_ids.selected.ids
   enable_deletion_protection = var.enable_deletion_protection
 }
--- elastic_application_balancer/variables.tf
+++ elastic_application_balancer/variables.tf
@@ -8,13 +8,15 @@ variable "load_balancer_type" {
   type        = string
   default = "application"
 }
-variable "security_groups" {
-  type        = list
-}
+
 variable "subnets_ids" {
   type        =  list
 }
 variable "enable_deletion_protection" {
   type        = bool
   default = false
+}
+
+variable "vpc_id" {
+  type        = string
 }
